e36899c34bee6a376aec8f19d00a42d6726eee17
Yauhen <penkin.evgeniy@gmail.com>
2017-02-13 15:43:19 +0300

    trouble with Fonts

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import { MainComponent } from './js/MainComponent';
 import { attachMainComponent } from './js/helpers';
+import './styles/style.scss';
+//import './node_modules/font-awesome/scss/font-awesome.scss';
 
 attachMainComponent(new MainComponent().render());
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 module.exports = {
 	entry: "./main",
-    output: {
-        path: __dirname + "/dist",
-        filename: "build.js"
-    },
-   module: {
+  output: {
+    path: __dirname + "/dist",
+    filename: "build.js"
+  },
+  module: {
     loaders: [
     {
       test: /\.js$/,
       exclude: /(node_modules|bower_components)/,
       loader: 'babel-loader?presets[]=es2015'
     },
-    {test: /\.css$/, loader: 'style-loader!css-loader'}
+    { test: /\.css$/, loader: 'style-loader!css-loader' },
+    {
+      test: /\.scss$/,
+      loader: 'style-loader!css-loader!sass-loader'
+    },
+    /*
+    {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml'},
+    {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
+    {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/font-woff"},
+    {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?mimetype=application/octet-stream"},
+    {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"}*/
+    {
+      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+      loader: 'file-loader'
+    }
     ]
   }
 }
