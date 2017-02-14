8553c10489598fb861d607d4f481ca1bb9286a58
Yauhen <penkin.evgeniy@gmail.com>
2017-02-14 13:51:30 +0300

    Resolve tab issue

--- js/helpers.js
+++ js/helpers.js
@@ -1,6 +1,6 @@
 import * as Handlebars from 'handlebars';
 export function attachMainComponent(component) {
-	document.getElementsByTagName('body')[0].appendChild(component);
+  document.getElementsByTagName('body')[0].appendChild(component);
 }
 
 export const handlebarsTernHelper = Handlebars.registerHelper('tern', function (value, equal, value1, value2) {
