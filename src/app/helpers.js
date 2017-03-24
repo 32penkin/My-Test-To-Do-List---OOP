a40f44d20d5344cb97d028c6b47b668ec9606269
Yauhen <penkin.evgeniy@gmail.com>
2017-03-24 08:33:29 +0300

    gh-pages

--- src/app/helpers.js
+++
@@ -1,5 +0,0 @@
-import * as Handlebars from 'handlebars';
-
-export const handlebarsTernHelper = Handlebars.registerHelper('tern', function (value, equal, value1, value2) {
-  return value === equal ? value1 : value2;
-});
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import {MainComponent} from './app/MainComponent';
-import './app/helpers';
-import './styles/style-fb.scss';
-import $ from 'jquery';
-
-$('body').html(new MainComponent().render());
